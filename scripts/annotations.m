a2e2d72ba64e9c98e1c91c28119b7d031c41e2d5
Quan Wen <qwen@fas.harvard.edu>
2024-01-01 22:57:28 +0800

    Update annotations.m

--- scripts/annotations.m
+++ scripts/annotations.m
@@ -13,11 +13,7 @@ y_original = y;
 Size = length(x);
 %Size = number of annotated neurons \times number of time points
 
-if isempty(tform_parameters)
-    
-    tform_parameters = h5read([data_directory,'/','data.h5'],'/tform_parameters');
-
-end
+tform_parameters = h5read([data_directory,'/','data.h5'],'/tform_parameters');
 
 for i = 1:Size
     delta_x = tform_parameters(t_idx(i)+1,1);
